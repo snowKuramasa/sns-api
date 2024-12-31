46c1db9560481b7e703d33f0b5fef666bed93e02
kuramasa <kuramasa.green@gmail.com>
2024-12-31 09:44:25 +0900

    cors設定を追加

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,34 @@ import express from 'express'
 import type { Express } from 'express'
 import userRoutes from './routes/userRoutes'
 import helloRoutes from './routes/helloRoutes'
+import cors from 'cors'
 
 const app: Express = express()
+
+// CORSの全体設定（アプリ全体に適用）
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',')
+
+// CORS設定
+const corsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+    if (!origin || allowedOrigins?.includes(origin)) {
+      // 許可されたオリジンからのリクエストを許可
+      callback(null, true)
+    } else {
+      // 許可されていないオリジンからのリクエストを拒否
+      callback(
+        new Error('CORSエラー: 許可されていないオリジンからのリクエストです')
+      )
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}
+
+// CORSミドルウェアの設定
+app.use(cors(corsOptions)) // アプリ全体に適用
 
 // ミドルウェア
 app.use(express.json()) // JSONパース
@@ -12,6 +38,6 @@ app.use(express.json()) // JSONパース
 app.use('/', helloRoutes)
 
 // ルート
-app.use('/api/user', userRoutes)
+app.use('/api/users', userRoutes)
 
 export default app
--- src/config/db.ts
+++ src/config/db.ts
@@ -5,9 +5,7 @@ dotenv.config()
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || '', {
-      dbName: 'sns-api', // データベース名
-    })
+    const conn = await mongoose.connect(process.env.MONGODB_URI || '')
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
     console.error('MongoDB connection failed:', error)
